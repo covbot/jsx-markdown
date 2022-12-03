ce5e8b3002bf91e1bd6809572f2c15dfc86da625
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-12-03 11:22:26 +0200

    Replaced module inlining with self-referencing in tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
 	transform: {
-		'\\.[jt]sx?$': ['ts-jest', { useESM: true }],
+		'\\.[t]sx?$': ['ts-jest', { useESM: true }],
 	},
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'cjs', 'mjs', 'json', 'node'],
 	collectCoverageFrom: ['<rootDir>/src/**/!(index).{ts,tsx,js,jsx,cjs,mjs}'],
