f1aac2df55b007a7991a13a707a47c56beaadef0
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2022-12-25 18:52:33 +0200

    Fix eslint issues

--- src/jsxTypes.ts
+++ src/jsxTypes.ts
@@ -1,10 +1,11 @@
 import { MarkdownElement, MarkdownIntrinsicElements } from './MarkdownIntrinsicElements';
 
 export declare namespace JSX {
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	type Element<T> = MarkdownElement;
 
 	interface ElementChildrenAttribute {
-		children: {}
+		children: {};
 	}
 
 	interface IntrinsicElements extends MarkdownIntrinsicElements {}
